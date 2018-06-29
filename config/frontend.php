82453d3fe409ac073cc3a955a7c272ed06f58848
Thomas Nadin <thomas.nadin@engageinteractive.co.uk>
2018-06-29 15:42:24 +0100

    Documents config/frontend.php

--- config/frontend.php
+++ config/frontend.php
@@ -4,20 +4,59 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Source File
+    | Enabled
     |--------------------------------------------------------------------------
     |
-    | Path of the source file from within resources/lang/:
+    | Turns the frontend routes on. You should ensure this is set to false in
+    | production environments.
     |
     */
     'enabled' => env('FRONTEND_ENABLED', false),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Route Name
+    |--------------------------------------------------------------------------
+    |
+    | Name prefix of the routes defined by the package, e.g.:
+    |
+    | route('frontend.index')
+    |
+    */
     'route_name' => 'frontend',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Route Path
+    |--------------------------------------------------------------------------
+    |
+    | URL path prefix of the  routes defined by the package, e.g.:
+    |
+    | /frontend/
+    |
+    */
     'route_path' => 'frontend',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Resource Path
+    |--------------------------------------------------------------------------
+    |
+    | Path prefix within the resources/views/ folder that the package looks for
+    | the frontend templates.
+    |
+    */
     'resource_path' => 'frontend',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Index Template Path
+    |--------------------------------------------------------------------------
+    |
+    | Path to the index template that lists out all the others, from the
+    | resources/views/ folder.
+    |
+    */
     'index_template_path' => 'app/frontend',
 
 ];
