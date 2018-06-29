8fab2ce84a37cc7ec97e1bfd84baaf2964154f21
Thomas Nadin <thomas.nadin@engageinteractive.co.uk>
2018-06-29 21:37:08 +0100

    Changes TemplateProvider to use File rather than Storage facade

--- tests/Unit/TemplateProviderTest.php
+++ tests/Unit/TemplateProviderTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Unit;
 
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 use Tests\TestCase;
 use Engage\LaravelFrontend\TemplateProvider;
@@ -11,8 +11,9 @@ class TemplateProviderTest extends TestCase
 {
     public function test_When_RetrievingTemplatesPaths_Then_StorageFacadeShouldBeUsed()
     {
-        Storage::fake('local');
-        Storage::put('resources/views/frontend/test.blade.php', '');
+        File::makeDirectory(resource_path('views/frontend'), 0755, $recursive = true, $force = true);
+        File::cleanDirectory(resource_path('views/frontend'));
+        File::put(resource_path('views/frontend/test.blade.php'), '');
 
         $this->assertEquals([ 'frontend/test' ], app(TemplateProvider::class)->allTemplatesPaths()->toArray());
     }
