2a6b832101848777356e131a850ba5332b86b2a2
Matthew Wilkinson <matthew-wilkinson@live.co.uk>
2020-04-19 17:55:09 +0100

    Migrates templates

---
+++ src/Templates/TemplateController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Engage\LaravelFrontend\Templates;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\View;
+use Engage\LaravelFrontend\Config;
+
+class TemplateController extends Controller
+{
+    /**
+     * Lists all frontend templates.
+     *
+     * @param \Engage\LaravelFrontend\Templates\TemplateService $service
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index(TemplateService $service)
+    {
+        $templates = $service->getTemplates();
+
+        return View::first([
+                'app/templates',
+                'frontend::frontend/index',
+            ])
+            ->with('templates', $templates)
+            ->with(Config::get('template_flag'), true)
+            ->with('links', [
+                'styleguide' => Config::get('show_styleguide', false),
+            ]);
+    }
+
+    /**
+     * Shows a frontend template.
+     *
+     * @param string  $template  relative path of the template.
+     * @param \Engage\LaravelFrontend\Templates\TemplateService $service
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function show(string $template, TemplateService $service)
+    {
+        return view($service->getTemplatePath($template))
+            ->with(Config::get('template_flag'), true);
+    }
+
+    /**
+     * Echo response endpoint that allows for custom status and json payload.
+     *
+     * @param \Illuminate\Http\Request
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function echo(Request $request)
+    {
+        $status = $request->query('status') ?: 200;
+        $json = $request->query('json');
+
+        if ($json) {
+            return response()->json($json, $status);
+        } else {
+            return response('', $status);
+        }
+    }
+}
---
+++ src/Templates/TemplateService.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Engage\LaravelFrontend\Templates;
+
+use Engage\LaravelFrontend\File\FileParser;
+use Engage\LaravelFrontend\Support\Path;
+use Engage\LaravelFrontend\Routing;
+use Engage\LaravelFrontend\Config;
+use Symfony\Component\Finder\Finder;
+
+class TemplateService
+{
+    /**
+     * Returns an array representation directory structure of templates.
+     *
+     * @return array
+     */
+    public function getTemplates(): array
+    {
+        $files = $this->getFinderFiles();
+
+        $templates = [];
+
+        foreach ($files as $file) {
+            $path = Path::withoutExtension($file->getRelativePathname());
+            $domain = Path::leadingName($path);
+            $templates[$domain][] = $this->processFile($file);
+        }
+
+        // Errors should always show last.
+        if (isset($templates['errors'])) {
+            $errors = $templates['errors'];
+            unset($templates['errors']);
+            $templates['errors'] = $errors;
+        }
+        
+        return $templates;
+    }
+
+    /**
+     * Generates the data structure for a template file.
+     *
+     * @param 
+     * 
+     * @return array
+     */
+    protected function processFile($file): array
+    {
+        $path = Path::withoutExtension($file->getRelativePathname());
+        $name = Path::withoutRoot($path);
+
+        $parser = new FileParser($file->getRealPath());
+        $meta = $parser->getMeta();
+
+        $templateData = [
+            'path' => Routing::getTemplateRoute($path),
+            'located' => $file->getRelativePathname(),
+            'name' => $name,
+        ];
+
+        return array_merge($templateData, $meta);
+    }
+
+    /**
+     * Searches and returns an istance of Finder with files from the 
+     * templates directory, sorted by name.
+     *
+     * @return \Symfony\Component\Finder\Finder
+     */
+    protected function getFinderFiles()
+    {
+        $resourcePath = Config::getAbsoluteResourcePath();
+        $pathPrefix = Path::trimTrailingSlash($resourcePath);
+        
+        return Finder::create()
+            ->files()
+            ->in($pathPrefix)
+            ->name('*.blade.php')
+            ->notName('_*')
+            ->notPath(Config::$excludeTemplateDirs)
+            ->sortByName();
+    }
+    
+    /**
+     * Returns the full template view path.
+     *
+     * @param string|null $template
+     * 
+     * @return string
+     */
+    public function getTemplatePath(string $template = null) : string
+    {
+        $resourcePath = Config::getRelativeResourcePath();
+        return Path::join($resourcePath, $template);
+    }
+}
