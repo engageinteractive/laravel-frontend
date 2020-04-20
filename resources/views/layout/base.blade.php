d1b5d1e98f13a0ee67abf279a13dfbaf167ee458
Matthew Wilkinson <matthew-wilkinson@live.co.uk>
2020-04-20 12:42:43 +0100

    Converts blade files indentation to tabs at the request of FE

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Templates</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+	<meta charset="utf-8">
+	<title>Templates</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 
-    @stack('stylesheets')
+	@stack('stylesheets')
 </head>
 <body>
-    <div id="app" class="relative">
-        @yield('app')
-    </div>
+	<div id="app" class="relative">
+		@yield('app')
+	</div>
 </body>
 </html>
